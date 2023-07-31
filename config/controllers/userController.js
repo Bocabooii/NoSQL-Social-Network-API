9bc0eea3a0b2f0bd5cb30ecd31f313d37baf2b64
Alexis Bocanegra <alexisbocanegra14@gmail.com>
2023-07-31 13:46:59 -0500

    Added Reaction.js file

---
+++ config/controllers/userController.js
@@ -0,0 +1,91 @@
+const { UserModel } = require('../models');
+
+const UserController = {
+  // Get all users
+  fetchAllUsers(req, res) {
+    UserModel.find({})
+      .then(users => res.json(users))
+      .catch(error => res.status(500).json(error));
+  },
+
+  // Get a user by ID
+  fetchUserById(req, res) {
+    UserModel.findById(req.params.userId)
+      .then(user => res.json(user))
+      .catch(error => res.status(500).json(error));
+  },
+  
+  // Create a new user
+  createUser(req, res) {
+    UserModel.create(req.body)
+      .then(newUser => res.json(newUser))
+      .catch(error => res.status(500).json(error));
+  },
+
+  // Update a user by ID
+  updateUserById(req, res) {
+    UserModel.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true }
+    )
+      .then(updatedUser => {
+        if (!updatedUser) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(updatedUser);
+      })
+      .catch(error => res.status(500).json(error));
+  },
+
+  // Delete a user by ID
+  deleteUserById(req, res) {
+    UserModel.findOneAndDelete({ _id: req.params.id })
+      .then(deletedUser => {
+        if (!deletedUser) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+        res.json({ message: 'User deleted successfully' });
+      })
+      .catch(error => res.status(500).json(error));
+  },
+
+  // Add a friend to the user's friend list
+  addFriend(req, res) {
+    UserModel.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.body.friendId || req.params.friendId } },
+      { new: true }
+    )
+      .then(updatedUser => {
+        if (!updatedUser) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(updatedUser);
+      })
+      .catch(error => res.status(500).json(error));
+  },
+
+  // Remove a friend from the user's friend list
+  removeFriend(req, res) {
+    UserModel.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    )
+      .then(updatedUser => {
+        if (!updatedUser) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+        const isFriendRemoved = !updatedUser.friends.includes(req.params.friendId);
+        if (isFriendRemoved) {
+          res.json({ message: 'Friend removed successfully!', updatedUser });
+        } else {
+          res.json(updatedUser);
+        }
+      })
+      .catch(error => res.status(400).json(error));
+  },
+};
+
+module.exports = UserController;
---
+++ models/Reaction.js
@@ -0,0 +1,32 @@
+const { Schema, Types } = require('mongoose');
+
+// Define the reaction schema using Mongoose Schema class
+const customReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        creatorUsername: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+module.exports = customReactionSchema;
